7fe24805d30d0f7d2c0af5d3d28ef3f9c921f262
Daniel Mauser <dmauser@hotmail.com>
2025-01-17 19:30:06 -0600

    update

--- 2-validation.sh
+++ 2-validation.sh
@@ -1,27 +1,5 @@
 
-###### BASTION SSH ######
-# Open a new terminal for each VM
-# Note: change the rg variable to the resource group name you used in the deployment
-# az-hub-lxvm
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name az-hub-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n az-hub-lxvm --query id -o tsv) --auth-type password --username $username
-# az-spk1-lxvm
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name az-hub-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n az-spk1-lxvm --query id -o tsv) --auth-type password --username $username
-# az-spk1-lxvm2
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name az-hub-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n az-spk1-lxvm2 --query id -o tsv) --auth-type password --username $username
-# az-spk2-lxvm
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name az-hub-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n az-spk2-lxvm --query id -o tsv) --auth-type password --username $username
-# onprem-lxvm
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name onprem-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n onprem-lxvm --query id -o tsv) --auth-type password --username $username
+
 
 
 # On az-spk1-lxvm run the following command to generate traffic to az-hub-lxvm:
--- 4-validation.sh
+++ 4-validation.sh
@@ -1,27 +1,3 @@
-###### BASTION SSH ######
-# Open a new terminal for each VM
-# Note: change the rg variable to the resource group name you used in the deployment
-# az-hub-lxvm
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name az-hub-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n az-hub-lxvm --query id -o tsv) --auth-type password --username $username
-# az-spk1-lxvm
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name az-hub-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n az-spk1-lxvm --query id -o tsv) --auth-type password --username $username
-# az-spk1-lxvm2
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name az-hub-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n az-spk1-lxvm2 --query id -o tsv) --auth-type password --username $username
-# az-spk2-lxvm
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name az-hub-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n az-spk2-lxvm --query id -o tsv) --auth-type password --username $username
-# onprem-lxvm
-rg=lab-vnet-encryption
-username=azureuser
-az network bastion ssh --name onprem-bastion --resource-group $rg --target-resource-id $(az vm show -g $rg -n onprem-lxvm --query id -o tsv) --auth-type password --username $username
-
 ### Check at the OS Level if the Accelerated Networking is enabled. That can be done in any VM on the hub and spoke networks
 # Example:
 sudo lspci
